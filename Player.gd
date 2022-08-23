a6eac52df0589af6c31e61f1fd81e0fb44719242
Matthew McCord <mattm.eternal@gmail.com>
2022-08-23 09:01:59 -0700

    video 2

--- Player.gd
+++ Player.gd
@@ -2,17 +2,45 @@ extends KinematicBody2D
 
 var velocity = Vector2.ZERO
 
+export(int) var ACCELERATION = 10
+export(int) var ADDITIONAL_FALL_GRAVITY = 2
+export(int) var FRICTION = 10
+export(int) var GRAVITY = 6
+export(int) var JUMP_FORCE = -180
+export(int) var JUMP_RELEASE_FORCE = -80
+export(int) var MAX_SPEED = 100
+
+
 func _physics_process(delta):
-	velocity.y += 4
+	apply_gravity()
 	
-	if Input.is_action_pressed("ui_right"):
-		velocity.x = 50
-	elif Input.is_action_pressed("ui_left"):
-		velocity.x = -50
+	var input = Vector2.ZERO
+	input.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	
+	if input.x == 0:
+		apply_friction()
 	else:
-		velocity.x = 0
+		apply_acceleration(input.x)
+			
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_up"):
+			velocity.y = JUMP_FORCE
+	else:
+		# If velocity.y < 0 we are going up. This stops the slight hang that happens releasing jump on the way down.
+		if Input.is_action_just_released("ui_up") and velocity.y < JUMP_RELEASE_FORCE: 
+			velocity.y = JUMP_RELEASE_FORCE
+			
+		if velocity.y > 1: # We are falling
+			velocity.y += ADDITIONAL_FALL_GRAVITY
+		
+	velocity = move_and_slide(velocity, Vector2.UP)
+
+
+func apply_gravity():
+	velocity.y += GRAVITY
 	
-	if Input.is_action_just_pressed("ui_up"):
-		velocity.y = -120
-	
-	velocity = move_and_slide(velocity)
+func apply_friction():
+	velocity.x = move_toward(velocity.x, 0, FRICTION)
+
+func apply_acceleration(amount):
+	velocity.x = move_toward(velocity.x, MAX_SPEED * amount, ACCELERATION)
