fd2f0099d15fb98bfbfa5f0e5b67809514bdc472
Matthew McCord <mattm.eternal@gmail.com>
2022-08-24 08:22:13 -0700

    video 3

--- Player.gd
+++ Player.gd
@@ -2,6 +2,8 @@ extends KinematicBody2D
 
 var velocity = Vector2.ZERO
 
+onready var animatedSprite = $AnimatedSprite
+
 export(int) var ACCELERATION = 10
 export(int) var ADDITIONAL_FALL_GRAVITY = 2
 export(int) var FRICTION = 10
@@ -11,7 +13,12 @@ export(int) var JUMP_RELEASE_FORCE = -80
 export(int) var MAX_SPEED = 100
 
 
-func _physics_process(delta):
+func _ready():
+	#animatedSprite.frames = load("res://resources/PlayerBlueSkin.tres")
+	animatedSprite.frames = load("res://resources/PlayerGreenSkin.tres")
+
+
+func _physics_process(_delta):
 	apply_gravity()
 	
 	var input = Vector2.ZERO
@@ -19,22 +26,34 @@ func _physics_process(delta):
 	
 	if input.x == 0:
 		apply_friction()
+		animatedSprite.animation = "Idle"
 	else:
 		apply_acceleration(input.x)
+		animatedSprite.animation = "Run"
+		if input.x > 0:
+			animatedSprite.flip_h = true
+		elif input.x < 0:
+			animatedSprite.flip_h = false
 			
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			velocity.y = JUMP_FORCE
 	else:
+		animatedSprite.animation = "Jump"
 		# If velocity.y < 0 we are going up. This stops the slight hang that happens releasing jump on the way down.
 		if Input.is_action_just_released("ui_up") and velocity.y < JUMP_RELEASE_FORCE: 
 			velocity.y = JUMP_RELEASE_FORCE
 			
 		if velocity.y > 1: # We are falling
 			velocity.y += ADDITIONAL_FALL_GRAVITY
-		
-	velocity = move_and_slide(velocity, Vector2.UP)
-
+	
+	var was_in_air = !is_on_floor()
+	velocity = move_and_slide(velocity, Vector2.UP) # is_on_floor will update when move_and_slide is called
+	var just_landed = is_on_floor() and was_in_air # if we were not on floor before move_and_slide and are after then we just landed :)
+	
+	if just_landed:
+		animatedSprite.animation = "Run"
+		animatedSprite.frame = 1
 
 func apply_gravity():
 	velocity.y += GRAVITY
